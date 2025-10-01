84c9f63663611cdb1e536db8f2e7b4afa092182d
IrfanCatovic <catovicc84@gmail.com>
2025-10-01 19:23:45 +0200

    functions newDeck & cards print

--- deck.go
+++ deck.go
@@ -1,7 +1,32 @@
 package main
 
+import "fmt"
+
 //Create a new type of 'deck
-// //which is a slice of strings 
+// //which is a slice of strings
 
 
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamons", "Hearts","Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits{
+		for _, value := range cardValues {
+			cards = append(cards, value + "of" + suit)
+		}
+		//ovde _ je upisano umesto indeksa "i" "j" ali posto ih nigde ne ispisujemo
+		//samo upisemo ovo da postoje, ali ne trebaju nam
+	}
+
+	return cards
+}
+
+func (d deck) print () {
+	for i, card:= range d{
+		fmt.Println(i,card)
+	}
+}
--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 	// card := "Ace of Spades"
 	// fmt.Println(card)
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	// cards := deck{"Ace of Diamonds", newCard()}
+	// cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards := newDeck()
+
+	cards.print()
+	// for i, card := range cards {
+	// 	fmt.Println(i, card)
+	// }
 }
 
-func newCard() string {
-	return "Five of Diamonds"
-}
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
